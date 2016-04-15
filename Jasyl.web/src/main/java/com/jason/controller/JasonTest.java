fba2bd7ef01c6d600f9937ea2ae3b5edbf5f56f4
cheng.zheng <cheng.zheng@tqmall.com>
2016-04-15 12:34:49 +0800

    jedis操作

--- Jasyl.web/src/main/java/com/jason/controller/JasonTest.java
+++ Jasyl.web/src/main/java/com/jason/controller/JasonTest.java
@@ -1,24 +1,40 @@
 package com.jason.controller;
 
+import com.jason.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import redis.clients.jedis.Jedis;
 
 /**
  * Created by jason on 16/4/5.
  */
 
 @Controller
-@RequestMapping("/pub/jason")
+@RequestMapping("/test")
 public class JasonTest {
 
-    @RequestMapping(value = "sylvia_sb")
-    @ResponseBody
-    public String test() {
+    @RequestMapping(value = "test")
+    public String test(Model model) {
+
+        User user = new User();
+        user.setId(1l);
+
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        jedis.set("name","jason");
+        System.out.println(jedis.get("name"));
+
+        model.addAttribute("a", jedis.get("name"));
+        return "test";
+
+    }
 
 
-        String str = "sylvia is a pig";
-        return str;
+    public static void main (String args[]) {
+        JasonTest jasonTest = new JasonTest();
+
+        jasonTest.test(null);
+
 
     }
 
