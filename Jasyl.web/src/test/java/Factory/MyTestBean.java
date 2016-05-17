7136a447b953a28af69d597887d4bc1688a56f5c
cheng.zheng <cheng.zheng@tqmall.com>
2016-05-17 17:00:28 +0800

    bean 测试

---
+++ Jasyl.web/src/test/java/Factory/MyTestBean.java
@@ -0,0 +1,18 @@
+package Factory;
+
+/**
+ * Created by jason on 16/5/11.
+ */
+public class MyTestBean {
+
+    private String testStr = "testStr";
+
+
+    public String getTestStr() {
+        return testStr;
+    }
+
+    public void setTestStr(String testStr) {
+        this.testStr = testStr;
+    }
+}
